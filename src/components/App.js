8f0f7ddd920120191e967d66b22549714e0ac1c1
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-14 08:45:49 -0700

    two form files added & browser router

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import { Route, Routes } from "react-router-dom"
 import '../App.css';
+import AddClientForm from './AddClientForm';
+import AddPolicyForm from './AddPolicyForm';
 
 export default function App() {
   return (
@@ -6,6 +10,12 @@ export default function App() {
       <header className="App-header">
         <h4>Welcome to the app!</h4>
       </header>
+      <Routes>
+        <Route path="/addclient" element={<AddClientForm />} />
+        <Route path="/addpolicy" element={<AddPolicyForm />} />
+
+      </Routes>
+      
     </div>
   );
 }
