c788751b93dbad52097ff65d0fb39f572e2802c0
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-14 20:06:03 -0700

    formatted clients in Client to render on ClientList comp

--- src/components/Client.js
+++ src/components/Client.js
@@ -2,4 +2,11 @@ import React from "react";
 
 export default function Client({ client }) {
 
+  return (
+    <>
+      <h3>{client.first_name} {client.last_name}</h3>
+      <p>Date of Birth: {client.date_of_birth}</p>
+      <p>Spouse: {client.spouse_name}</p>
+    </>
+  )
 }
