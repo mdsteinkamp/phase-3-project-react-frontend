199ff7cb519511036332a87fa9bd7dca30e09e14
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-14 20:13:55 -0700

    added client state p tag in Client

--- src/components/Client.js
+++ src/components/Client.js
@@ -6,6 +6,7 @@ export default function Client({ client }) {
     <>
       <h3>{client.first_name} {client.last_name}</h3>
       <p>Date of Birth: {client.date_of_birth}</p>
+      <p>State: {client.state}</p>
       <p>Spouse: {client.spouse_name}</p>
     </>
   )
