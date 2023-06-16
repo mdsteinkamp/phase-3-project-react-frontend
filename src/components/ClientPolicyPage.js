0ca119a4ec38890baa08ed09ee6809bb75160d99
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-15 20:29:23 -0700

    removed React Strict mode from index.js

--- src/components/ClientPolicyPage.js
+++ src/components/ClientPolicyPage.js
@@ -8,16 +8,22 @@ export default function ClientPolicyPage({ clients }) {
   const client = clients.find(client => client.id === parseInt(id))
 
   useEffect(() => {
-    fetch(`http://localhost:9292/policies/${client.id}`)
+    fetch(`http://localhost:9292/clients/${client.id}`)
       .then(resp => resp.json())
-      .then((client) => setClientWithPolicies(client));
-  }, [client]);
-
-  console.log(clientWithPolicies)
+      .then((client) => setClientWithPolicies(client))
+    }, []);
+    
+    console.log(clientWithPolicies.policies)
+  
 
   return (
     <>
-      <h3>client policy page</h3>
+      <h3>Client Policy List</h3>
+      <h3>{client.first_name} {client.last_name}</h3>
+      <p>Carrier: {clientWithPolicies.carrier}</p>
+      <p>Policy Number: {clientWithPolicies.policy_number}</p>
+      <p>Face Amount: {clientWithPolicies.face_amount}</p>
+      <p>Policy Date: {clientWithPolicies.policy_date}</p>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
